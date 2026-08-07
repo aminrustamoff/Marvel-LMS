ac4f02cf4f22868e40cfa182ecc85b99e11bb5ff
Aminjon <aminrustamov0424@gmail.com>
2026-08-07 17:22:11 +0500

    Set up MVP video podcast models

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ListeningPodcast
 
-# Register your models here.
+@admin.register(ListeningPodcast)
+class ListeningPodcastAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'created_by', 'created_at')
+    search_fields = ('title', 'description', 'url')
+    list_filter = ('created_by', 'created_at')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk or obj.created_by is None:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
