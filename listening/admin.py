9599d6dc3e76bc8d8613b10966ffcbfc828f76fd
Aminjon <aminrustamov0424@gmail.com>
2026-08-07 16:45:51 +0500

    Set up MVP listening models

--- listening/admin.py
+++ listening/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Listening, ListeningImages
 
-# Register your models here.
+
+class ListeningImagesInline(admin.TabularInline):
+    model = ListeningImages
+    extra = 1
+
+
+@admin.register(Listening)
+class ListeningAdmin(admin.ModelAdmin):
+    list_display = ("title", "part", "created_at")
+    list_filter = ("part", "created_at")
+    search_fields = ("title", "question", "answer")
+    inlines = (ListeningImagesInline,)
