cf64b50802922d9227f4653e5f0ee6fba587fd78
Aminjon <aminrustamov0424@gmail.com>
2026-09-09 18:05:37 +0500

    Add mian home page to the web page

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,11 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
+from home.views import home
+
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="accounts:login", permanent=False)),
+    # path("", RedirectView.as_view(pattern_name="accounts:login", permanent=False)),
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("groups/", include("groups.urls")),
